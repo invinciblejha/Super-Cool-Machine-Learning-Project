e01fd1de6e83a613d02b94fd6543716c1083a3ee
Myles Loffler <myles2007@gmail.com>
2012-01-13 15:50:37 -0600

    Get all movies is now defined.

--- scrape_rotten_tomatoes.py
+++ scrape_rotten_tomatoes.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2.7
 import json
 import requests
+import urllib
 from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker
 
@@ -23,18 +24,44 @@ class APIRequest(object):
         contents = req.content
         return json.loads(contents) if load_json else contents
 
-    def get_movies_in_theaters(self, page_limit='', page='', country='', **kwargs):
-        return self.__make_request('/lists/movies/in_theaters.json?page_limit=%s&page=%s&country=%s' % (page_limit, page, country), **kwargs)
+    #def get_movies_in_theaters(self, page_limit='', page='', country='', **kwargs):
+    #    movies = self.__make_request('/lists/movies/in_theaters.json?page_limit=%s&page=%s&country=%s' % (page_limit, page, country), **kwargs)
+    #    for moviedict in movies['movies']:
+    #        movie = Movie(id=moviedict['id'], name=moviedict['title'])
+    #        self.session.add(movie)
+    #    self.session.commit()
 
-    def get_movies(self, query='', page_limit='', page='', **kwargs):
-        enc_query = requests.quote_plus(query)
-        movies = self.__make_request('/movies.json?q=%s&page_limit=%s&page=%s' % (enc_query, page_limit, page), **kwargs)
-        for moviedict in movies:
-            movie = Movie(moviedict['id'],moviedict['title'])
+    def get_movies(self, uri, **kwargs):
+        movies = self.__make_request(uri, **kwargs)
+        for moviedict in movies['movies']:
+            movie = Movie(id=moviedict['id'], name=moviedict['title'])
             self.session.add(movie)
         self.session.commit()
+
+    def get_all_movies(self, page_limit='', page='', country='', **kwargs):
+        types = (('in_theaters', 'movies'), ('upcoming', 'movies'), ('current_releases', 'dvds'))
+
+        for selection, type_ in types:
+            uri = '/lists/%s/%s.json?page_limit=%s&page=%s&country=%s' % (type_, selection, page_limit, page, country)
+            self.get_movies(uri, **kwargs)
+
+    def get_movies_by_query(self, query='', page_limit='', page='', **kwargs):
+        enc_query = urllib.quote_plus(query)
+        movies = self.__make_request('/movies.json?q=%s&page_limit=%s&page=%s' % (enc_query, page_limit, page), **kwargs)
+        print movies
+        for moviedict in movies['movies']:
+            movie = Movie(id=moviedict['id'], name=moviedict['title'])
+            self.session.add(movie)
+        self.session.commit()
+
+    def _print_movie_titles(self):
+        for movie in self.session.query(Movie).all():
+            print movie.name
 
 if __name__ == '__main__':
     req = APIRequest(settings.API_KEY)
 #     print req.get_movies('Toy Story 3')
-    print req.get_movies_in_theaters(load_json=False)
+    #req.get_movies_in_theaters()
+    req.get_all_movies()
+    req._print_movie_titles()
+
