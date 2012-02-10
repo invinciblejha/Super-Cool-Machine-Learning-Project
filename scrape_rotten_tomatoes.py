511e5b28cf8783da655dbd6836b598bcfa50f69f
Ray Gregory <greatgrandpadog@gmail.com>
2012-02-10 16:12:54 -0600

    Added support for multiple pages. Added ability to not error on duplicat key insertion.

--- scrape_rotten_tomatoes.py
+++ scrape_rotten_tomatoes.py
@@ -18,6 +18,9 @@ class APIRequest(object):
         self.engine = create_engine('sqlite:///tomatoes.db')
         Movie.metadata.bind = self.engine
         self.session = sessionmaker(bind=self.engine)()
+        Movie.__table__.create(checkfirst=True)
+
+        self.last_request = None
 
     def __make_request(self, uri, load_json=True):
         req = requests.get(''.join([settings.BASE_API_URI, uri, '&apikey=', settings.API_KEY]))
@@ -33,17 +36,39 @@ class APIRequest(object):
 
     def get_movies(self, uri, **kwargs):
         movies = self.__make_request(uri, **kwargs)
+
+        if movies == self.last_request:
+            return 0 
+        self.last_request = movies
+        print 'movies: %s' % (movies)
+        movie_ids_already_present = [y.id for y in self.session.query(Movie).filter(Movie.id.in_(x['id'] for x in movies['movies']))]
         for moviedict in movies['movies']:
             movie = Movie(id=moviedict['id'], name=moviedict['title'])
-            self.session.add(movie)
-        self.session.commit()
+            if int(movie.id) not in movie_ids_already_present:
+#                 print 'found one. {0}'.format(movie.id)
+                self.session.add(movie)
 
-    def get_all_movies(self, page_limit='', page='', country='', **kwargs):
+        self.session.commit()
+        return len(movies['movies']) 
+
+    def get_all_movies(self, page_limit='', country='', **kwargs):
+        
         types = (('in_theaters', 'movies'), ('upcoming', 'movies'), ('current_releases', 'dvds'))
 
         for selection, type_ in types:
-            uri = '/lists/%s/%s.json?page_limit=%s&page=%s&country=%s' % (type_, selection, page_limit, page, country)
-            self.get_movies(uri, **kwargs)
+            page = 1
+            self.last_request = None
+            keep_going = True
+            movies_found = 0
+            while keep_going:
+                uri = '/lists/%s/%s.json?page_limit=%s&page=%s&country=%s' % (type_, selection, page_limit, page, country)
+                cur_movies_found = self.get_movies(uri, **kwargs)
+                keep_going = cur_movies_found > 0
+                movies_found += cur_movies_found
+                print 'read page: {0}'.format(page)
+                page += 1
+
+            print 'movies found for seclect {0} type {1}: {2}'.format(selection, type_, movies_found)
 
     def get_movies_by_query(self, query='', page_limit='', page='', **kwargs):
         enc_query = urllib.quote_plus(query)
@@ -55,8 +80,11 @@ class APIRequest(object):
         self.session.commit()
 
     def _print_movie_titles(self):
-        for movie in self.session.query(Movie).all():
+        movie_list = self.session.query(Movie).all()
+        for movie in movie_list: 
             print movie.name
+        
+        print 'movie count: {0}'.format(len(movie_list)) 
 
 if __name__ == '__main__':
     req = APIRequest(settings.API_KEY)
